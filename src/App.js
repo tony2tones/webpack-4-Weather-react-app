733bea2d5e982c5eccfd01787fe5ae46468c808c
tony2tones <anthony.marque@gmail.com>
2018-07-09 16:36:56 +0200

    Added apiKey and BaseURL links in App

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
-import React from "react";
+import React, { Component } from 'react';
 import ReactDOM from "react-dom";
+
+const apiKEY = '53f9d8e4213222cf517d86dc406d67fc';
+const baseURL = 'http://api.openweathermap.org/data/2.5/weather';
 
 const App = () => {
     return (
