fca4c1e01217c6b6e97ca1c09bc1635ee6f4f952
tony2tones <anthony.marque@gmail.com>
2018-07-09 15:19:45 +0200

    added more packages specifically loaders,updated my webpack config file

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,23 @@ module.exports = {
                 use: {
                     loader: "babel-loader"
                 }
+            },
+            ,
+            {
+                test: /\.html$/,
+                use: [
+                    {
+                        loader: "html-loader",
+                        options: { minimize: true }
+                    }
+                ]
             }
         ]
-    }
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: "./src/index.html",
+            filename: "./index.html"
+        })
+    ]
 };
