f996aaa22efe131b232e4c3fcfc8fdbf76379669
Anthony <anthonymarques@outlook.com>
2018-07-09 18:41:25 +0200

    Added functions GeoSuccess and GeoFail

--- src/App.js
+++ src/App.js
@@ -9,47 +9,70 @@ class App extends Component {
     constructor() {
         super();
         this.state = {
-          showWeather: false,
-          broken: false,
-          isLoading: true,
-          latitude: null,
-          longitude: null,
-          error: null,
-          showError: false,
-          weather: {
-            cTemp: '',
-            fTemp: '',
-            weatherNiceName: '',
-            cTempMin: '',
-            cTempMax: '',
-            fTempMin: '',
-            fTempMax: '',
-            location: '',
-            icon: '',
-          },
+            showWeather: false,
+            broken: false,
+            isLoading: true,
+            latitude: null,
+            longitude: null,
+            error: null,
+            showError: false,
+            weather: {
+                cTemp: '',
+                fTemp: '',
+                weatherNiceName: '',
+                cTempMin: '',
+                cTempMax: '',
+                fTempMin: '',
+                fTempMax: '',
+                location: '',
+                icon: '',
+            },
         };
-      }
+    }
 
-      omponentDidMount() {
+    omponentDidMount() {
         // Get location, and cater for if location is provided
         const getLocation = ({ latitude, longitude }) => {
-          request
-            .get(App.apiUrl(latitude, longitude))
-            .set('accept', 'json')
-            .then((res) => {
-              this.mapData(res.body);
-            })
-            .catch(() => {
-              // err.message, err.response
-            });
+            request
+                .get(App.apiUrl(latitude, longitude))
+                .set('accept', 'json')
+                .then((res) => {
+                    this.mapData(res.body);
+                })
+                .catch(() => {
+                    // err.message, err.response
+                });
         };
+
+        // This is if user accept location permission
+        const geoSuccess = ({ coords }) => {
+            this.setState({ showError: false });
+            getLocation(coords);
+        }
+
+        // If user declines location permission
+        const geoFail = () => {
+            this.setState({ showError: true, isLoading: false })
+        }
+
+        navigator.geolocation.getCurrentPosition(
+            (position) => {
+                this.setState({
+                    latitude: position.coords.latitude,
+                    longitude: position.coords.longitude,
+                    error: null,
+                });
+            },
+            error => this.setState({ error: error.message }),
+            { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 },
+        );
     };
 
     render() {
         return (
-        <div>
-            <p>React here! Toasty</p>
-        </div>
+            <div>
+                <p>React here! Toasty</p>
+            </div>
         )
     };
 };
