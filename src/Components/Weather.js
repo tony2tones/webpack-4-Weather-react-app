04098d80dfe0746faea261164c10ae102df6072a
tony2tones <anthony.marque@gmail.com>
2018-07-10 16:11:00 +0200

    styling card component

--- src/Components/Weather.js
+++ src/Components/Weather.js
@@ -5,7 +5,7 @@ import "./Weather.css";
 const Weather = (props) => (
     <div className="wrapper">
         <div className="card">
-            <img src='https://www.farmersalmanac.com/wp-content/uploads/2009/08/blueMoon2-600x398.jpg' alt="moon image" width="50%"></img>
+            <img src='https://www.farmersalmanac.com/wp-content/uploads/2009/08/blueMoon2-600x398.jpg' alt="moon image" width="400px"></img>
             <h4>{props.location}</h4>
             <p>{props.fTemp}</p>
         </div>
