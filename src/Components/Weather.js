1f4886674354b253bda1de8982de7e66843e44e1
tony2tones <anthony.marque@gmail.com>
2018-07-10 16:21:50 +0200

    trying to add Location label on image

--- src/Components/Weather.js
+++ src/Components/Weather.js
@@ -6,7 +6,7 @@ const Weather = (props) => (
     <div className="wrapper">
         <div className="card">
             <img src='https://www.farmersalmanac.com/wp-content/uploads/2009/08/blueMoon2-600x398.jpg' alt="moon image" width="400px"></img>
-            <h4>{props.location}</h4>
+            <h4 className="centered">{props.location}</h4>
             <p>{props.fTemp}</p>
         </div>
     </div>
