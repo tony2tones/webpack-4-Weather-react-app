def7e05b1d508c1588af0c32d698c5cbefd82f95
tony2tones <anthony.marque@gmail.com>
2018-07-10 15:18:52 +0200

    building up Weather card

--- src/Components/Weather.js
+++ src/Components/Weather.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Weather = (props) => (
 
     <div className="card">
-        <img src='https://www.farmersalmanac.com/wp-content/uploads/2009/08/blueMoon2-600x398.jpg' alt="moon image" width="50%"></img>
+        <img src='https://www.farmersalmanac.com/wp-content/uploads/2009/08/blueMoon2-600x398.jpg' alt="moon image" width="25%"></img>
         <h1>{props.location}</h1>
         <p>{props.fTemp}</p>
     </div>
