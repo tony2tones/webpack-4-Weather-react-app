8accc1aca7894943557ec8761848b59022fe2717
tony2tones <anthony.marque@gmail.com>
2018-07-10 16:45:36 +0200

    Adding more styling

--- src/Components/Weather.js
+++ src/Components/Weather.js
@@ -6,11 +6,10 @@ const Weather = (props) => (
     <div className="wrapper">
         <div className="card">
             <img src='https://www.farmersalmanac.com/wp-content/uploads/2009/08/blueMoon2-600x398.jpg' alt="moon image" width="400px"></img>
-            <h4 className="centered">{props.location}</h4>
+            <h3 className="centered">{props.location}</h3>
             <p>{props.fTemp}</p>
         </div>
     </div>
-
 );
 
 export default Weather;
