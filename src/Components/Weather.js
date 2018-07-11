8a574a9f0bb385b836504c8e55d5eb81c17faee0
tony2tones <anthony.marque@gmail.com>
2018-07-11 15:17:50 +0200

    still messing with some UI stuff

--- src/Components/Weather.js
+++ src/Components/Weather.js
@@ -5,10 +5,9 @@ import "./Weather.css";
 const Weather = (props) => (
     <div className="wrapper">
         <div className="card">
-            <img src='https://www.farmersalmanac.com/wp-content/uploads/2009/08/blueMoon2-600x398.jpg' alt="moon image" width="410px" height="400px"></img>
+            <img src='https://www.farmersalmanac.com/wp-content/uploads/2009/08/blueMoon2-600x398.jpg' alt="moon image" width="100%" height="400px"></img>
             <h4 className="topper">{props.location}</h4> 
             <h3 className="centered">{props.cTemp}°</h3>
-            <p>{props.weatherNiceName}</p>
             <p>{props.cTemp}</p>
             <p><button>REFRESH</button></p>
         </div>
