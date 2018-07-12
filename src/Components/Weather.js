f8402e68113ed15c87d91c9a0471003ea9086171
tony2tones <anthony.marque@gmail.com>
2018-07-12 15:58:45 +0200

    still working on them stylings

--- src/Components/Weather.js
+++ src/Components/Weather.js
@@ -5,12 +5,12 @@ import "./Weather.css";
 const Weather = (props) => (
     <div className="wrapper">
         <div className="card">
-            <img src='https://www.farmersalmanac.com/wp-content/uploads/2009/08/blueMoon2-600x398.jpg' alt="moon image" width="100%" height="400px"></img>
+            <img src='https://www.farmersalmanac.com/wp-content/uploads/2009/08/blueMoon2-600x398.jpg' alt="moon image" width="400px" height="400px"></img>
             <div className="location">{props.location}</div> 
             <div className="topper2">{props.weatherNiceName}</div>
             <div className="centered">{props.cTemp}°</div>
             <p>{props.cTempMax}° /{props.cTempMin}°</p>
-            <p><button>refresh</button></p>
+            <p><button>REFRESH</button></p>
         </div>
     </div>
 );
