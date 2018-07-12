b4729561afbcc8f542d3c9e7422ece4377777145
tony2tones <anthony.marque@gmail.com>
2018-07-12 16:47:52 +0200

    still playing around with styling

--- src/Components/Weather.js
+++ src/Components/Weather.js
@@ -6,14 +6,14 @@ const Weather = (props) => (
     <div className="wrapper">
         <div className="card">
             <div>
-                <img src='https://www.farmersalmanac.com/wp-content/uploads/2009/08/blueMoon2-600x398.jpg' alt="moon image" width="400px" height="320px"></img>
+                <img src='https://www.farmersalmanac.com/wp-content/uploads/2009/08/blueMoon2-600x398.jpg' alt="moon image" width="400px" height="300px"></img>
             </div>
             <div className="location">{props.location}</div>
             <div className="topper2">{props.weatherNiceName}</div>
             <div className="centered">{props.cTemp}°</div>
             <p>{props.cTempMax}° /{props.cTempMin}°</p>
             <p>Gibberish</p>
-            <button className="buttonStyling">REFRESH</button>
+            <button>REFRESH</button>
             
         </div>
     </div>
