8a4bfddb108ab9c0d6c7accf81e25d9834e0e66a
tony2tones <anthony.marque@gmail.com>
2018-07-12 16:30:39 +0200

    styling button tricky business

--- src/Components/Weather.js
+++ src/Components/Weather.js
@@ -5,12 +5,16 @@ import "./Weather.css";
 const Weather = (props) => (
     <div className="wrapper">
         <div className="card">
-            <img src='https://www.farmersalmanac.com/wp-content/uploads/2009/08/blueMoon2-600x398.jpg' alt="moon image" width="400px" height="400px"></img>
-            <div className="location">{props.location}</div> 
+            <div>
+                <img src='https://www.farmersalmanac.com/wp-content/uploads/2009/08/blueMoon2-600x398.jpg' alt="moon image" width="400px" height="320px"></img>
+            </div>
+            <div className="location">{props.location}</div>
             <div className="topper2">{props.weatherNiceName}</div>
             <div className="centered">{props.cTemp}°</div>
             <p>{props.cTempMax}° /{props.cTempMin}°</p>
-            <p><button>REFRESH</button></p>
+            <p>Gibberish</p>
+            <button className="buttonStyling">REFRESH</button>
+            
         </div>
     </div>
 );
