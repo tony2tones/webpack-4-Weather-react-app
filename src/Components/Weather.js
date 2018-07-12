9ba8fc8f6d3afd9f7ebd025a8cd168a7dcee85d8
tony2tones <anthony.marque@gmail.com>
2018-07-12 16:52:47 +0200

    Need to leave this styling alone now

--- src/Components/Weather.js
+++ src/Components/Weather.js
@@ -6,7 +6,7 @@ const Weather = (props) => (
     <div className="wrapper">
         <div className="card">
             <div>
-                <img src='https://www.farmersalmanac.com/wp-content/uploads/2009/08/blueMoon2-600x398.jpg' alt="moon image" width="400px" height="300px"></img>
+                <img src='https://www.farmersalmanac.com/wp-content/uploads/2009/08/blueMoon2-600x398.jpg' alt="moon image" width="370px" height="300px"></img>
             </div>
             <div className="location">{props.location}</div>
             <div className="topper2">{props.weatherNiceName}</div>
