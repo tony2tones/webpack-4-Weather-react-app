aac7d23e702ccd104bb16ac0dbb79f7a535185f5
tony2tones <anthony.marque@gmail.com>
2018-07-12 17:00:59 +0200

    added refresh button

--- src/Components/Weather.js
+++ src/Components/Weather.js
@@ -9,12 +9,11 @@ const Weather = (props) => (
                 <img src='https://www.farmersalmanac.com/wp-content/uploads/2009/08/blueMoon2-600x398.jpg' alt="moon image" width="370px" height="300px"></img>
             </div>
             <div className="location">{props.location}</div>
-            <div className="topper2">{props.weatherNiceName}</div>
+            <div className="WeatherName">{props.weatherNiceName}</div>
             <div className="centered">{props.cTemp}°</div>
             <p>{props.cTempMax}° /{props.cTempMin}°</p>
             <p>Gibberish</p>
-            <button>REFRESH</button>
-            
+            <button onClick={() => window.location.reload()}>REFRESH</button>
         </div>
     </div>
 );
