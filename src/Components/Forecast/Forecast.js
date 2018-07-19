4143057f85fe125e5f91c4ea68d77586c8bcfc80
tony2tones <anthony.marque@gmail.com>
2018-07-19 15:53:55 +0200

    Working on Stylings

--- src/Components/Forecast/Forecast.js
+++ src/Components/Forecast/Forecast.js
@@ -5,25 +5,25 @@ import './forecast.css';
 const Forecast = (props) => {
     return (
         <div>
-            <p>Later today</p>
+            <div>Later today</div>
             <div className="grid">
                 <div>
 
-                    <p>{props.temp1}</p>
+                    <p className="temp">{props.temp1}°</p>
                     <div className="time">
                         {props.time1}
                     </div>
                 </div>
                 <div>
 
-                    <p>{props.temp2}</p>
+                    <p  className="temp">{props.temp2}°</p>
                     <div className="time">
                         {props.time2}
                     </div>
                 </div>
                 <div>
 
-                    <p id="item3">{props.temp3}</p>
+                    <p  className="temp">{props.temp3}°</p>
                     <div className="time">
                         {props.time3}
                     </div>
