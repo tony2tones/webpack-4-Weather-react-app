df8889be8da92d5b606b6c3ea56d29616c843b3a
tony2tones <anthony.marque@gmail.com>
2018-08-06 13:37:41 +0200

    Added jest test file

---
+++ .jest/test-setup.js
@@ -0,0 +1,9 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+
+import { configure } from 'enzyme';
+
+// eslint-disable-next-line import/no-extraneous-dependencies
+
+import Adapter from 'enzyme-adapter-react-16';
+
+configure({ adapter: new Adapter() });
