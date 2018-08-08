38a732e121d8231f881963c4a8d0587caf5fce16
tony2tones <anthony.marque@gmail.com>
2018-08-08 12:15:28 +0200

    Adding unit tests for my weather app

--- src/Components/Weather/Weather.js
+++ src/Components/Weather/Weather.js
@@ -6,7 +6,7 @@ const Weather = (props) => (
     <div>
         <div>{props.time}</div>
         <img src='../src/assets/img/darkimg.png' alt="moon image" width="370px" height="300px"></img>
-        <div className="location">{props.location}</div>
+        <div className="location" data-qa="weather__location">{props.location}</div>
         <div className="WeatherName">{props.weatherNiceName}</div>
         <div className="centered">{props.cTemp}°</div>
     </div>
--- src/Components/Weather/weather.test.js
+++ src/Components/Weather/weather.test.js
@@ -5,20 +5,20 @@ import Weather from './Weather';
 
 // const C_TEMP = 'C_TEMP';
 const LOCATION = 'LOCATION';
-// const WEATHERNICENAME = 'WEATHERNICENAME';
+const WEATHERNICENAME = 'WEATHERNICENAME';
 // const C_TEMP_MIN = 'C_TEMP_MIN';
 // const WEATHER_NAME = 'WEATHER_NAME';
 // const ICON = 'ICON';
 
 test('should render an `location to exist`', () => {
-  const wrapper = shallow(<Weather />);
+  const wrapper = shallow(<Weather 
     location={LOCATION}
     // cTemp={C_TEMP}
     // cTempMax={WEATHERNICENAME}
     // cTempMin={C_TEMP_MIN}
-    // WEATHERNICENAME={WEATHER_NAME}
+    WEATHERNICENAME={WEATHERNICENAME}
     // icon={ICON}
-//   />);
+  />);
   const expected = true;
 
   const actual = wrapper.find('.location').exists();
@@ -26,33 +26,33 @@ test('should render an `location to exist`', () => {
   expect(actual).toBe(expected);
 });
 
-// test('Passes the props through correctly', () => {
-//   const wrapper = shallow(<Weather
-//     location={LOCATION}
+test('Passes the props through correctly', () => {
+  const wrapper = shallow(<Weather
+    location={LOCATION}
 //     cTemp={C_TEMP}
 //     cTempMax={WEATHERNICENAME}
 //     cTempMin={C_TEMP_MIN}
 //     WEATHERNICENAME={WEATHER_NAME}
 //     icon={ICON}
-//   />);
-//   const expectedLocation = LOCATION;
+  />);
+  const expectedLocation = LOCATION;
 //   const expectedCTemp = `${C_TEMP}°C`;
 //   const expectedMaxCTemp = `${WEATHERNICENAME}°C`;
 //   const expectedMinCTemp = `${C_TEMP_MIN}°C`;
 //   const expectedWeatherName = WEATHER_NAME;
 
-//   const actualLocation = wrapper.find('[data-qa="weather__location"]').text();
+  const actualLocation = wrapper.find('[data-qa="weather__location"]').text();
 //   const actualCTemp = wrapper.find('[data-qa="weather__temperature__celcius"]').text();
 //   const actualMaxCTemp = wrapper.find('[data-qa="weather__temperature__celcius__max"]').text();
 //   const actualMinCTemp = wrapper.find('[data-qa="weather__temperature__celcius__min"]').text();
 //   const actualWeatherName = wrapper.find('[data-qa="weather__name"]').text();
 
-//   expect(actualLocation).toBe(expectedLocation);
+  expect(actualLocation).toBe(expectedLocation);
 //   expect(actualCTemp).toBe(expectedCTemp);
 //   expect(actualMaxCTemp).toBe(expectedMaxCTemp);
 //   expect(actualMinCTemp).toBe(expectedMinCTemp);
 //   expect(actualWeatherName).toBe(expectedWeatherName);
-// });
+});
 
 // test('should check for classname iconHeader', () => {
 //   const wrapper = shallow(<Weather
