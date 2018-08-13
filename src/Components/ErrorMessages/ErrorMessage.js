e9150cfa9712555821e7909c4d8de20b499adcf0
tony2tones <anthony.marque@gmail.com>
2018-08-13 09:04:43 +0200

    added unit tests for errorMessage component

--- src/Components/ErrorMessages/ErrorMessage.js
+++ src/Components/ErrorMessages/ErrorMessage.js
@@ -7,7 +7,7 @@ const ErrorMessage = () => {
         <div className="wrapper">
             <div className="card">
                 <img src='../src/assets/img/mushie-worries2.png' alt="moon image" width="370px" height="300px"></img>
-                <span>Please allow us to use your location for the latest weather updates</span>
+                <span>Please allow us to use your location for the latest weather updates.</span>
             </div>
         </div>
     )
--- src/Components/ErrorMessages/errorMessage.test.js
+++ src/Components/ErrorMessages/errorMessage.test.js
@@ -3,3 +3,11 @@ import { shallow } from 'enzyme';
 
 import ErrorMessage from './ErrorMessage';
 
+test('should have the following error message', () => {
+    const wrapper = shallow(<ErrorMessage />);
+    const expected = 'Please allow us to use your location for the latest weather updates.';
+
+    const actual = wrapper.find('span').text();
+
+    expect(actual).toBe(expected);
+});
