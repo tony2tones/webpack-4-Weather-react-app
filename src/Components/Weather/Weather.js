8cf61ae514a83a78745b0ce9ebf9a5d93147263d
Anthony <anthonymarques@outlook.com>
2018-10-10 20:49:27 +0200

    adding a weather type component to add images using switch case

--- src/Components/Weather/Weather.js
+++ src/Components/Weather/Weather.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
 import "./Weather.css";
+import WeatherType from './WeatherType/WeatherType';
 
 const Weather = (props) => (
-    let weatherType = props.weatherNiceName;
     <div>
-        {console.log(weatherType)}
+        <WeatherType
+            weatherType={props.weatherNiceName}
+        />
         <div>{props.time}</div>
         <img src='../src/assets/img/darkimg.png' alt="moon image" width="370px" height="300px"></img>
         <div className="location" data-qa="weather__location">{props.location}</div>
---
+++ src/Components/Weather/WeatherType/WeatherType.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const WeatherType = (props) => {
+    var weatherType = props.weatherType;
+    {console.log(weatherType)} 
+    return (
+        <div> some weather text </div>
+    )
+    
+}
+
+export default WeatherType;
