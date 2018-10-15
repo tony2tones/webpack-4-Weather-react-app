1143efe863a2b0adfdba8d195f56c000f2358ee2
Anthony <anthonymarques@outlook.com>
2018-10-15 18:29:15 +0200

    added switch case

--- src/Components/Weather/Weather.js
+++ src/Components/Weather/Weather.js
@@ -5,11 +5,12 @@ import WeatherType from './WeatherType/WeatherType';
 
 const Weather = (props) => (
     <div>
+        
+        <div>{props.time}</div>
         <WeatherType
             weatherType={props.weatherNiceName}
         />
-        <div>{props.time}</div>
-        <img src='../src/assets/img/darkimg.png' alt="moon image" width="370px" height="300px"></img>
+        {/* <img src='../src/assets/img/darkimg.png' alt="moon image" width="370px" height="300px"></img> */}
         <div className="location" data-qa="weather__location">{props.location}</div>
         <div className="WeatherName" data-qa="weather__name">{props.weatherNiceName}</div>
         <div className="centered" data-qa="centered__temp">{props.cTemp}°</div>
--- src/Components/Weather/WeatherType/WeatherType.js
+++ src/Components/Weather/WeatherType/WeatherType.js
@@ -1,12 +1,26 @@
-import React from 'react';
+import React from "react";
 
-const WeatherType = (props) => {
-    var weatherType = props.weatherType;
-    {console.log(weatherType)} 
-    return (
-        <div> some weather text </div>
-    )
-    
-}
+const WeatherType = props => {
+  var weatherType = props.weatherType;
+// var weatherType = "LIGHT INTENSITY DRIZZLE RAIN";
+  var text;
+  {
+    console.log(weatherType);
+  }
+  return (
+    <div>
+      {(() => {
+        switch (weatherType) {
+          case "LIGHT INTENSITY DRIZZLE RAIN":
+            text = "https://upload.wikimedia.org/wikipedia/commons/2/29/Big_Fat_Red_Cat.jpg";
+            break;
+          case "BROKEN CLOUDS":
+            text = "../src/assets/img/darkimg.png";
+        }
+      })()}
+      <img src={text} width="370px" height="300px"></img>
+    </div>
+  );
+};
 
 export default WeatherType;
